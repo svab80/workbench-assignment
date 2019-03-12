5601f06072fcb5b138b1e8d6a255e2a167820e82
svab80 <svenar80@hotmail.com>
2019-03-12 21:53:48 +0100

    Completed guessing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,5 +1,18 @@
- 
- 
- 
- 
- 
+#!/usr/bin/env bash 
+
+files=$(ls -l | egrep "^-" | wc -l)
+
+read -p "How many files are there in the current directory? : " guess
+
+while [ $guess -ne $files ]
+do
+	if [ $guess -gt $files ]
+	then
+		read -p "Your guess was too high, try again: " guess
+	else
+		read -p "Your guess was too low, try again: " guess
+	fi
+
+done
+
+echo "You guessed right, there is $files in this directory!"
